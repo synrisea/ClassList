812d3ff4689f95e22759cd66a87ade29d05078e2
synrisea <150649087+synrisea@users.noreply.github.com>
2023-11-30 01:00:01 +0400

    PushBack ; PopFront ; PopBack
    -PushBack : method which is adding an element to the end of list.
    -PopFront : method which is deleting an element from the beginning of the list.
    -PopBack: method which is deleting an element from the end of the list.

--- Lists/Lists/myList.cpp
+++ Lists/Lists/myList.cpp
@@ -19,21 +19,93 @@ private:
 
 public:
 
+	// Constructors : 
+
 	List() : first(nullptr), last(nullptr) {}
 
 	List(std::vector<T>& v) : first(nullptr), last(nullptr)
 	{
-		for (auto i = v.rbegin(); i != v.rend(); i++)
+		for (auto i = v.begin(); i != v.end(); i++)
 		{
 			PushBack(*i);
 		}
 	}
 
+	// Methods : 
 
 	bool isEmpty() const
 	{
 		return first == nullptr;
 	}
+
+
+	void PushBack(const T& newElement)
+	{
+		Node* element = new Node;
+
+		if (isEmpty())
+		{
+			element->next = nullptr;
+			element->prev = nullptr;
+			element->data = newElement;
+
+			first = last = element;
+		}
+		else
+		{
+			element->next = nullptr;
+			element->prev = last;
+			element->data = newElement;
+
+			last->next = element;
+
+			last = element;
+		}
+	}
+
+
+	void PopFront()
+	{
+		if (first != nullptr)
+		{
+			Node* element = first;
+
+			first = first->next;
+
+			if (first != nullptr)
+			{
+				first->prev = nullptr;
+			}
+			else
+			{
+				last = nullptr;  // If first was the only node, then we are updating last.
+			}
+
+			delete element;
+		}
+	}
+
+	void PopBack()
+	{
+		if (last != nullptr)
+		{
+			Node* element = last;
+
+			last = last->prev;
+
+			if (last != nullptr)
+			{
+				last->next = nullptr;
+			}
+			else
+			{
+				first = nullptr;  // If last was the only node, then we are updating first.
+			}
+
+			delete element;
+		}
+	}
+
 };
 
 int main()
